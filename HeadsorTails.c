2e6aa06cbdf8b9a8638f64c5cf20e03cf39d7424
cheVll <87257017+cheVll@users.noreply.github.com>
2022-07-14 22:03:38 +0900

    Final revision

--- HeadsorTails.c
+++ HeadsorTails.c
@@ -3,6 +3,12 @@
 
 int main(){
     int Rand = 0, Heads = 0, Tails = 0;
+    char name[100];
+
+    printf("Who are you?\n");
+    scanf("%s", &name);
+    printf("Hello, %s!\n",name);
+
     printf("Tossing a coin...\n");
     for(int i = 1; i <= 3; i++){
         printf("Round %d: ", i);
@@ -18,8 +24,8 @@ int main(){
     }
     printf("Heads: %d, Tails: %d\n", Heads, Tails);
     if(Heads > Tails){
-        printf("You won\n");
+        printf("%s won!\n", name);
     }else{
-        printf("You lost\n");
+        printf("%s lost!\n", name);
     }
 }
