3dcdbc5956665f18b91b5fdc17b637cdd3d972b6
cheVll <87257017+cheVll@users.noreply.github.com>
2022-07-14 19:03:06 +0900

    User name added

---
+++ Usernameadded.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int Rand = 0, Heads = 0, Tails = 0;
+    char name[100];
+
+    printf("Who are you?\n");
+    scanf("%s", &name);
+    printf("Hello, %s!\n",name);
+    
+    printf("Tossing a coin...\n");
+    for(int i = 1; i <= 3; i++){
+        printf("Round %d: ", i);
+        Rand = rand()% 2;
+        if(Rand == 0){
+            printf("Heads\n");
+            Heads++;
+        }
+        else{
+            printf("Tails\n");
+            Tails++;
+        }
+    }
+    printf("Heads: %d, Tails: %d\n", Heads, Tails);
+}
